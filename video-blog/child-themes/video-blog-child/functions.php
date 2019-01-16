beffd3c8b0f76a451b54912ed7f696af13d78598
vinhnq <vinhnq12@gmail.com>
2019-01-16 11:56:55 +0700

    update child theme video-blog

---
+++ video-blog/child-themes/video-blog-child/functions.php
@@ -0,0 +1,10 @@
+<?php
+function thim_child_enqueue_styles() {
+	if ( is_multisite() ) {
+		wp_enqueue_style( 'thim-child-style', get_stylesheet_uri() );
+	} else {
+		wp_enqueue_style( 'thim-parent-style', get_template_directory_uri() . '/style.css' );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'thim_child_enqueue_styles', 100 );
